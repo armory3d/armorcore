734479787bdb6ee7b900cb2ef5e854318a0d56d2
luboslenco <lubos.lenco@gmail.com>
2019-10-30 12:48:29 +0100

    Add android and ios to kincfile

--- kincfile.js
+++ kincfile.js
@@ -4,38 +4,44 @@ const with_d3dcompiler = true;
 const with_nfd = true;
 const with_audio = false;
 
-let project = new Project('Krom');
-project.cpp11 = true;
-project.addFile('Sources/main.cpp');
-project.addIncludeDir('V8/include');
-project.addDefine('KINC_IMAGE_STANDARD_MALLOC');
-
-let system = platform === Platform.Windows ? "win32" :
-			 platform === Platform.Linux ? "linux" : "macos";
+const system = platform === Platform.Windows ? "win32" :
+			   platform === Platform.Linux ? "linux" :
+			   platform === Platform.Android ? "android" :
+			   platform === Platform.iOS ? "ios" : "macos";
 
 const build = release ? 'release' : 'debug';
 const libdir = 'V8/Libraries/' + system + '/' + build + '/';
 
+let project = new Project('Krom');
+project.cpp11 = true;
+project.setDebugDir('Deployment');
+project.addFile('Sources/main.cpp');
+project.addIncludeDir('V8/include');
+project.addDefine('KINC_IMAGE_STANDARD_MALLOC');
+
 if (platform === Platform.Windows) {
 	project.addLib('Dbghelp'); // Stack walk
 	project.addLib(libdir + 'v8_monolith');
+	if (with_d3dcompiler) {
+		project.addDefine('WITH_D3DCOMPILER');
+		project.addLib("d3d11");
+		project.addLib("d3dcompiler");
+	}
 }
 else if (platform === Platform.Linux) {
 	project.addLib('v8_monolith -L../../' + libdir);
+}
+else if (platform === Platform.Android) {
+	project.addLib(libdir + 'libv8_monolith.a');
+}
+else if (platform === Platform.iOS) {
+	project.addLib('libv8_monolith.a');
 }
 else if (platform === Platform.OSX) {
 	project.addLib('libv8_monolith.a');
 }
 
-project.setDebugDir('Deployment');
-
-if (with_d3dcompiler && platform === Platform.Windows) {
-	project.addDefine('WITH_D3DCOMPILER');
-	project.addLib("d3d11");
-	project.addLib("d3dcompiler");
-}
-
-if (with_nfd) {
+if (with_nfd && (platform === Platform.Windows || platform === Platform.Linux || platform === Platform.OSX)) {
 	project.addDefine('WITH_NFD');
 	project.addIncludeDir("Libraries/nfd/include");
 	project.addFile('Libraries/nfd/nfd_common.c');
