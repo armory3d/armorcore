183ee9b43723a407fda30d7cfce98a4f3b767af4
luboslenco <lubos.lenco@gmail.com>
2020-04-16 11:37:48 +0200

    Merge drawCubicBezier

--- Sources/kha/graphics2/GraphicsExtension.hx
+++ Sources/kha/graphics2/GraphicsExtension.hx
@@ -35,4 +35,50 @@ class GraphicsExtension {
 			g2.fillTriangle(px, py, x + cx, y + cy, cx, cy);
 		}
 	}
+
+	/**
+	 * Draws a cubic bezier using 4 pairs of points. If the x and y arrays have a length bigger then 4, the additional
+	 * points will be ignored. With a length smaller of 4 a error will occur, there is no check for this.
+	 * You can construct the curves visually in Inkscape with a path using default nodes.
+	 * Provide x and y in the following order: startPoint, controlPoint1, controlPoint2, endPoint
+	 * Reference: http://devmag.org.za/2011/04/05/bzier-curves-a-tutorial/
+	 */
+	public static function drawCubicBezier(g2:Graphics, x:Array<Float>, y:Array<Float>, segments:Int = 20, strength:Float = 1.0):Void {
+		var t:Float;
+
+		var q0 = calculateCubicBezierPoint(0, x, y);
+		var q1:Array<Float>;
+
+		for (i in 1...(segments + 1)) {
+			t = i / segments;
+			q1 = calculateCubicBezierPoint(t, x, y);
+			g2.drawLine(q0[0], q0[1], q1[0], q1[1], strength);
+			q0 = q1;
+		}
+	}
+
+	static function calculateCubicBezierPoint(t:Float, x:Array<Float>, y:Array<Float>):Array<Float> {
+		var u:Float = 1 - t;
+		var tt:Float = t * t;
+		var uu:Float = u * u;
+		var uuu:Float = uu * u;
+		var ttt:Float = tt * t;
+
+		// first term
+		var p:Array<Float> = [uuu * x[0], uuu * y[0]];
+
+		// second term
+		p[0] += 3 * uu * t * x[1];
+		p[1] += 3 * uu * t * y[1];
+
+		// third term
+		p[0] += 3 * u * tt * x[2];
+		p[1] += 3 * u * tt * y[2];
+
+		// fourth term
+		p[0] += ttt * x[3];
+		p[1] += ttt * y[3];
+
+		return p;
+	}
 }
