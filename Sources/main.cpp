d7f98a8460157c1f786eefd1ee5989ad101dec62
Lubos Lenco <lubos.lenco@gmail.com>
2022-01-03 16:06:23 +0100

    Use new vertex data types

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -656,22 +656,82 @@ namespace {
 
 	kinc_g4_vertex_data_t convert_vertex_data(int num) {
 		switch (num) {
-		case 0:
-			return KINC_G4_VERTEX_DATA_FLOAT1;
-		case 1:
-			return KINC_G4_VERTEX_DATA_FLOAT2;
-		case 2:
-			return KINC_G4_VERTEX_DATA_FLOAT3;
-		case 3:
-			return KINC_G4_VERTEX_DATA_FLOAT4;
-		case 4:
-			return KINC_G4_VERTEX_DATA_FLOAT4X4;
-		case 5:
-			return KINC_G4_VERTEX_DATA_SHORT2_NORM;
-		case 6:
-			return KINC_G4_VERTEX_DATA_SHORT4_NORM;
+		case 0: // Float32_1X
+			return KINC_G4_VERTEX_DATA_F32_1X;
+		case 1: // Float32_2X
+			return KINC_G4_VERTEX_DATA_F32_2X;
+		case 2: // Float32_3X
+			return KINC_G4_VERTEX_DATA_F32_3X;
+		case 3: // Float32_4X
+			return KINC_G4_VERTEX_DATA_F32_4X;
+		case 4: // Float32_4X4
+			return KINC_G4_VERTEX_DATA_F32_4X4;
+		case 5: // Int8_1X
+			return KINC_G4_VERTEX_DATA_I8_1X;
+		case 6: // UInt8_1X
+			return KINC_G4_VERTEX_DATA_U8_1X;
+		case 7: // Int8_1X_Normalized
+			return KINC_G4_VERTEX_DATA_I8_1X_NORMALIZED;
+		case 8: // UInt8_1X_Normalized
+			return KINC_G4_VERTEX_DATA_U8_1X_NORMALIZED;
+		case 9: // Int8_2X
+			return KINC_G4_VERTEX_DATA_I8_2X;
+		case 10: // UInt8_2X
+			return KINC_G4_VERTEX_DATA_U8_2X;
+		case 11: // Int8_2X_Normalized
+			return KINC_G4_VERTEX_DATA_I8_2X_NORMALIZED;
+		case 12: // UInt8_2X_Normalized
+			return KINC_G4_VERTEX_DATA_U8_2X_NORMALIZED;
+		case 13: // Int8_4X
+			return KINC_G4_VERTEX_DATA_I8_4X;
+		case 14: // UInt8_4X
+			return KINC_G4_VERTEX_DATA_U8_4X;
+		case 15: // Int8_4X_Normalized
+			return KINC_G4_VERTEX_DATA_I8_4X_NORMALIZED;
+		case 16: // UInt8_4X_Normalized
+			return KINC_G4_VERTEX_DATA_U8_4X_NORMALIZED;
+		case 17: // Int16_1X
+			return KINC_G4_VERTEX_DATA_I16_1X;
+		case 18: // UInt16_1X
+			return KINC_G4_VERTEX_DATA_U16_1X;
+		case 19: // Int16_1X_Normalized
+			return KINC_G4_VERTEX_DATA_I16_1X_NORMALIZED;
+		case 20: // UInt16_1X_Normalized
+			return KINC_G4_VERTEX_DATA_U16_1X_NORMALIZED;
+		case 21: // Int16_2X
+			return KINC_G4_VERTEX_DATA_I16_2X;
+		case 22: // UInt16_2X
+			return KINC_G4_VERTEX_DATA_U16_2X;
+		case 23: // Int16_2X_Normalized
+			return KINC_G4_VERTEX_DATA_I16_2X_NORMALIZED;
+		case 24: // UInt16_2X_Normalized
+			return KINC_G4_VERTEX_DATA_U16_2X_NORMALIZED;
+		case 25: // Int16_4X
+			return KINC_G4_VERTEX_DATA_I16_4X;
+		case 26: // UInt16_4X
+			return KINC_G4_VERTEX_DATA_U16_4X;
+		case 27: // Int16_4X_Normalized
+			return KINC_G4_VERTEX_DATA_I16_4X_NORMALIZED;
+		case 28: // UInt16_4X_Normalized
+			return KINC_G4_VERTEX_DATA_U16_4X_NORMALIZED;
+		case 29: // Int32_1X
+			return KINC_G4_VERTEX_DATA_I32_1X;
+		case 30: // UInt32_1X
+			return KINC_G4_VERTEX_DATA_U32_1X;
+		case 31: // Int32_2X
+			return KINC_G4_VERTEX_DATA_I32_2X;
+		case 32: // UInt32_2X
+			return KINC_G4_VERTEX_DATA_U32_2X;
+		case 33: // Int32_3X
+			return KINC_G4_VERTEX_DATA_I32_3X;
+		case 34: // UInt32_3X
+			return KINC_G4_VERTEX_DATA_U32_3X;
+		case 35: // Int32_4X
+			return KINC_G4_VERTEX_DATA_I32_4X;
+		case 36: // UInt32_4X
+			return KINC_G4_VERTEX_DATA_U32_4X;
 		}
-		return KINC_G4_VERTEX_DATA_FLOAT1;
+		return KINC_G4_VERTEX_DATA_NONE;
 	}
 
 	void krom_create_vertexbuffer(const FunctionCallbackInfo<Value> &args) {
@@ -713,7 +773,7 @@ namespace {
 		int count = args[2]->Int32Value(isolate->GetCurrentContext()).FromJust();
 		float *vertices = kinc_g4_vertex_buffer_lock(buffer, start, count);
 		Local<ArrayBuffer> abuffer = ArrayBuffer::New(isolate, vertices, (count - start) * kinc_g4_vertex_buffer_stride(buffer));
-		args.GetReturnValue().Set(Float32Array::New(abuffer, 0, (count - start) * kinc_g4_vertex_buffer_stride(buffer) / 4));
+		args.GetReturnValue().Set(abuffer);
 	}
 
 	void krom_unlock_vertex_buffer(const FunctionCallbackInfo<Value> &args) {
