04f15fa8c51a1c0d5fad3740969afaf4b189f086
luboslenco <lubos.lenco@gmail.com>
2023-03-10 18:44:32 +0100

    Add .obj parser

---
+++ Sources/iron/io_obj.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct obj_part {
+	int16_t *posa;
+	int16_t *nora;
+	int16_t *texa;
+	uint32_t *inda;
+	int vertex_count;
+	int index_count;
+	float scale_pos;
+	float scale_tex;
+	char *name;
+	bool has_next; // File contains multiple objects
+	size_t pos;
+	uint32_t **udims; // Indices split per udim tile
+	int *udims_count;
+	int udims_u; // Number of horizontal udim tiles
+	int udims_v;
+} obj_part_t;
+
+obj_part_t *io_obj_parse(char *file_bytes, char split_code, int start_pos, bool udim);
+void io_obj_destroy(obj_part_t *part);
