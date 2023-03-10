62e82707cfa90a951b8dc33c8ed0cc7c564f9bd3
luboslenco <lubos.lenco@gmail.com>
2023-03-10 12:52:57 +0100

    Comment out iron_app code

--- Sources/iron/iron_app.c
+++ Sources/iron/iron_app.c
@@ -1,4 +1,6 @@
 
+#include <kinc/window.h>
+
 int iron_app_w() { return kinc_window_width(0); }
 int iron_app_h() { return kinc_window_height(0); }
 int iron_app_x() { return 0; }
@@ -18,14 +20,14 @@ int iron_last_w = -1;
 int iron_last_h = -1;
 void (*iron_on_resize)(void) = NULL;
 
-void iron_app_init(void (*done)(void)) {
-	iron_app_new(done);
-}
-
 void iron_app_new(void (*done)(void)) {
 	done();
 	// kha.System.notifyOnFrames(render);
 	// kha.Scheduler.addTimeTask(update, 0, iron.system.Time.delta);
+}
+
+void iron_app_init(void (*done)(void)) {
+	iron_app_new(done);
 }
 
 void iron_app_reset() {
@@ -38,10 +40,10 @@ void iron_app_reset() {
 }
 
 void iron_app_update() {
-	if (iron_scene_active() == NULL || !iron_scene_active()->ready) return;
-	if (iron_pause_updates) return;
+	// if (iron_scene_active() == NULL || !iron_scene_active()->ready) return;
+	// if (iron_pause_updates) return;
 
-	iron_scene_active()->update_frame();
+	// iron_scene_active()->update_frame();
 
 	// int i = 0;
 	// int l = trait_updates.length;
@@ -67,7 +69,7 @@ void iron_app_update() {
 	// if (on_end_frames != NULL) for (f in on_end_frames) f();
 
 	// Rebuild projection on window resize
-	if (iron_last_w == -1) {
+	/*if (iron_last_w == -1) {
 		iron_last_w = iron_app_w();
 		iron_last_h = iron_app_h();
 	}
@@ -80,11 +82,11 @@ void iron_app_update() {
 		}
 	}
 	iron_last_w = iron_app_w();
-	iron_last_h = iron_app_h();
+	iron_last_h = iron_app_h();*/
 }
 
-void iron_app_render(kinc_framebuffer **frames) {
-	kinc_framebuffer frame = frames[0];
+// void iron_app_render(kinc_framebuffer_t **frames) {
+	/*kinc_framebuffer frame = frames[0];
 	framebuffer = frame;
 
 	iron_time_update();
@@ -92,7 +94,7 @@ void iron_app_render(kinc_framebuffer **frames) {
 	if (iron_scene_active() == NULL || !iron_scene_active()->ready) {
 		render2d(frame);
 		return;
-	}
+	}*/
 
 	// if (trait_inits.length > 0) {
 	// 	for (f in trait_inits) {
@@ -101,16 +103,16 @@ void iron_app_render(kinc_framebuffer **frames) {
 	// 	trait_inits.splice(0, trait_inits.length);
 	// }
 
-	iron_scene_active()->render_frame(frame.g4);
+	// iron_scene_active()->render_frame(frame.g4);
 
 	// for (f in trait_renders) {
 	// 	trait_renders.length > 0 ? f(frame.g4) : break;
 	// }
 
-	render2d(frame);
-}
+	// render2d(frame);
+// }
 
-void iron_app_render2d(kinc_framebuffer *frame) {
+// void iron_app_render2d(kinc_framebuffer_t *frame) {
 	// if (trait_renders2d.length > 0) {
 	// 	frame.g2.begin(false);
 	// 	for (f in trait_renders2d) {
@@ -118,10 +120,10 @@ void iron_app_render2d(kinc_framebuffer *frame) {
 	// 	}
 	// 	frame.g2.end();
 	// }
-}
+// }
 
 // Hooks
-void iron_app_notify_on_init(void (*f)(void)) {
+/*void iron_app_notify_on_init(void (*f)(void)) {
 	// trait_inits.push(f);
 }
 
@@ -145,19 +147,19 @@ void iron_app_remove_late_update(void (*f)(void)) {
 	// trait_late_updates.remove(f);
 }
 
-void iron_app_notify_on_render(void (*f)(kinc_graphics4 *)) {
+void iron_app_notify_on_render(void (*f)(kinc_graphics4_t *)) {
 	// trait_renders.push(f);
 }
 
-void iron_app_remove_render(void (*f)(kinc_graphics4 *)) {
+void iron_app_remove_render(void (*f)(kinc_graphics4_t*)) {
 	// trait_renders.remove(f);
 }
 
-void iron_app_notify_on_render2d(void (*f)(kinc_graphics2 *)) {
+void iron_app_notify_on_render2d(void (*f)(kinc_graphics2_t*)) {
 	// trait_renders2d.push(f);
 }
 
-void iron_app_remove_render2d(void (*f)(kinc_graphics2 *)) {
+void iron_app_remove_render2d(void (*f)(kinc_graphics2_t*)) {
 	// trait_renders2d.remove(f);
 }
 
@@ -177,4 +179,4 @@ void notify_on_end_frame(void (*f)(void)) {
 
 void remove_end_frame(void (*f)(void)) {
 	// on_end_frames.remove(f);
-}
+}*/
