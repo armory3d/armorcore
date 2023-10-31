79b9315e5f1c530487b097257e57b368de9feba0
luboslenco <lubos.lenco@gmail.com>
2023-10-31 20:21:54 +0100

    Iron string stubs

--- Sources/iron/iron_string.h
+++ Sources/iron/iron_string.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include <stdint.h>
+
+typedef struct string {
+	uint8_t *data;
+	int length;
+} string_t;
+
+void string_init(string_t *s, const char *str);
+void string_free(string_t *s);
