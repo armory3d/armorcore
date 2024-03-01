1d6b76ecc3e1400157d62488dc83f52998932077
luboslenco <lubos.lenco@gmail.com>
2024-03-01 12:57:23 +0100

    Implement iron maps

--- Sources/iron/iron_map.c
+++ Sources/iron/iron_map.c
@@ -2,3 +2,27 @@
 #include "iron_map.h"
 #define STB_DS_IMPLEMENTATION
 #include <stb_ds.h>
+
+void i32_map_set(i32_map_t *m, char *k, int v) {
+	shput(m->hash, k, v);
+}
+
+void any_map_set(any_map_t *m, char *k, void *v) {
+	shput(m->hash, k, v);
+}
+
+int32_t i32_map_get(i32_map_t *m, void *k) {
+	return shget(m->hash, k);
+}
+
+void *any_map_get(any_map_t *m, void *k) {
+	return shget(m->hash, k);
+}
+
+void map_delete(any_map_t *m, void *k) {
+
+}
+
+void *map_to_array(any_map_t *m) {
+	return NULL;
+}
--- Sources/iron/iron_map.h
+++ Sources/iron/iron_map.h
@@ -1,5 +1,20 @@
 #pragma once
 
-typedef struct map {
-	char *data;
-} map_t;
+#include <stdint.h>
+
+typedef struct i32_map {
+	struct { char *key; int value; } *hash;
+} i32_map_t;
+
+typedef struct any_map {
+	struct { char *key; void *value; } *hash;
+} any_map_t;
+
+void i32_map_set(i32_map_t *m, char *k, int v);
+void any_map_set(any_map_t *m, char *k, void *v);
+
+int32_t i32_map_get(i32_map_t *m, void *k);
+void *any_map_get(any_map_t *m, void *k);
+
+void map_delete(any_map_t *m, void *k);
+void *map_to_array(any_map_t *m);
