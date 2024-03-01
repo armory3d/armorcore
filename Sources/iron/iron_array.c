7c353963e07ab5239691f657abe1577291b4cb1c
luboslenco <lubos.lenco@gmail.com>
2024-03-01 12:57:06 +0100

    Implement iron arrays

--- Sources/iron/iron_array.c
+++ Sources/iron/iron_array.c
@@ -57,7 +57,144 @@ void f32_array_push(f32_array_t *a, float e) {
 	a->buffer[a->length++] = e;
 }
 
-void any_array_push(any_array_t *a, uintptr_t e) {
+void any_array_push(any_array_t *a, void *e) {
 	array_alloc(a, sizeof(uintptr_t));
 	a->buffer[a->length++] = e;
+}
+
+void i8_array_resize(i8_array_t *a, int32_t size) {
+	a->capacity = size;
+	a->buffer = realloc(a->buffer, a->capacity * sizeof(int8_t));
+}
+
+void u8_array_resize(u8_array_t *a, int32_t size) {
+	a->capacity = size;
+	a->buffer = realloc(a->buffer, a->capacity * sizeof(uint8_t));
+}
+
+void i16_array_resize(i16_array_t *a, int32_t size) {
+	a->capacity = size;
+	a->buffer = realloc(a->buffer, a->capacity * sizeof(int16_t));
+}
+
+void u16_array_resize(u16_array_t *a, int32_t size) {
+	a->capacity = size;
+	a->buffer = realloc(a->buffer, a->capacity * sizeof(uint16_t));
+}
+
+void i32_array_resize(i32_array_t *a, int32_t size) {
+	a->capacity = size;
+	a->buffer = realloc(a->buffer, a->capacity * sizeof(int32_t));
+}
+
+void u32_array_resize(u32_array_t *a, int32_t size) {
+	a->capacity = size;
+	a->buffer = realloc(a->buffer, a->capacity * sizeof(uint32_t));
+}
+
+void f32_array_resize(f32_array_t *a, int32_t size) {
+	a->capacity = size;
+	a->buffer = realloc(a->buffer, a->capacity * sizeof(float));
+}
+
+void any_array_resize(any_array_t *a, int32_t size) {
+	a->capacity = size;
+	a->buffer = realloc(a->buffer, a->capacity * sizeof(void *));
+}
+
+void buffer_resize(buffer_t *b, int32_t size) {
+	b->length = size;
+	b->data = realloc(b->data, b->length * sizeof(uint8_t));
+}
+
+void array_sort(void *ar, void *fn) {
+
+}
+
+void array_push(void *ar, void *e) {
+
+}
+
+void array_splice(void *ar, int32_t start, int32_t delete_count) {
+
+}
+
+void *array_concat(void *a, void *b) {
+	return NULL;
+}
+
+void *array_slice(void *a, int32_t begin, int32_t end) {
+	return NULL;
+}
+
+void array_remove(void *ar, void *e) {
+
+}
+
+buffer_t *buffer_slice(buffer_t *a, int32_t begin, int32_t end) {
+	return NULL;
+}
+
+int32_t buffer_size(buffer_t *b) {
+	return b->length;
+}
+
+int32_t buffer_view_size(buffer_view_t *v) {
+	return v->buffer->length;
+}
+
+uint8_t buffer_view_get_u8(buffer_view_t *v, int32_t p) {
+	return *(uint8_t *)(v->buffer->data + p);
+}
+
+int8_t buffer_view_get_i8(buffer_view_t *v, int32_t p) {
+	return *(int8_t *)(v->buffer->data + p);
+}
+
+uint16_t buffer_view_get_u16(buffer_view_t *v, int32_t p) {
+	return *(uint16_t *)(v->buffer->data + p);
+}
+
+int16_t buffer_view_get_i16(buffer_view_t *v, int32_t p) {
+	return *(int16_t *)(v->buffer->data + p);
+}
+
+uint32_t buffer_view_get_u32(buffer_view_t *v, int32_t p) {
+	return *(uint32_t *)(v->buffer->data + p);
+}
+
+int32_t buffer_view_get_i32(buffer_view_t *v, int32_t p) {
+	return *(int32_t *)(v->buffer->data + p);
+}
+
+float buffer_view_get_f32(buffer_view_t *v, int32_t p) {
+	return *(float *)(v->buffer->data + p);
+}
+
+void buffer_view_set_u8(buffer_view_t *v, int32_t p, uint8_t n) {
+	*(uint8_t *)(v->buffer->data + p) = n;
+}
+
+void buffer_view_set_i8(buffer_view_t *v, int32_t p, int8_t n) {
+	*(int8_t *)(v->buffer->data + p) = n;
+}
+
+void buffer_view_set_u16(buffer_view_t *v, int32_t p, uint16_t n) {
+	*(uint16_t *)(v->buffer->data + p) = n;
+}
+
+void buffer_view_set_i16(buffer_view_t *v, int32_t p, uint16_t n) {
+	*(int16_t *)(v->buffer->data + p) = n;
+}
+
+void buffer_view_set_u32(buffer_view_t *v, int32_t p, uint32_t n) {
+	*(uint32_t *)(v->buffer->data + p) = n;
+}
+
+void buffer_view_set_i32(buffer_view_t *v, int32_t p, int32_t n) {
+	*(int32_t *)(v->buffer->data + p) = n;
+}
+
+void buffer_view_set_f32(buffer_view_t *v, int32_t p, float n) {
+	*(float *)(v->buffer->data + p) = n;
 }
--- Sources/iron/iron_array.h
+++ Sources/iron/iron_array.h
@@ -45,13 +45,25 @@ typedef struct f32_array {
 } f32_array_t;
 
 typedef struct any_array {
-	uintptr_t *buffer;
+	void **buffer;
 	int length;
 	int capacity;
 } any_array_t;
 
-typedef struct buffer {} buffer_t;
-typedef struct buffer_view {} buffer_view_t;
+typedef struct char_ptr_array {
+	char **buffer;
+	int length;
+	int capacity;
+} char_ptr_array_t;
+
+typedef struct buffer {
+	uint8_t *data;
+	int length;
+} buffer_t;
+
+typedef struct buffer_view {
+	buffer_t *buffer;
+} buffer_view_t;
 
 void array_free(void *a);
 void i8_array_push(i8_array_t *a, int8_t e);
@@ -61,4 +73,39 @@ void u16_array_push(u16_array_t *a, uint16_t e);
 void i32_array_push(i32_array_t *a, int32_t e);
 void u32_array_push(u32_array_t *a, uint32_t e);
 void f32_array_push(f32_array_t *a, float e);
-void any_array_push(any_array_t *a, uintptr_t e);
+void any_array_push(any_array_t *a, void *e);
+
+void i8_array_resize(i8_array_t *a, int32_t size);
+void u8_array_resize(u8_array_t *a, int32_t size);
+void i16_array_resize(i16_array_t *a, int32_t size);
+void u16_array_resize(u16_array_t *a, int32_t size);
+void i32_array_resize(i32_array_t *a, int32_t size);
+void u32_array_resize(u32_array_t *a, int32_t size);
+void f32_array_resize(f32_array_t *a, int32_t size);
+void any_array_resize(any_array_t *a, int32_t size);
+void buffer_resize(buffer_t *b, int32_t size);
+
+void array_sort(void *ar, void *fn);
+void array_push(void *ar, void *e);
+void array_splice(void *ar, int32_t start, int32_t delete_count);
+void *array_concat(void *a, void *b);
+void *array_slice(void *a, int32_t begin, int32_t end);
+void array_remove(void *ar, void *e);
+
+buffer_t *buffer_slice(buffer_t *a, int32_t begin, int32_t end);
+int32_t buffer_size(buffer_t *b);
+int32_t buffer_view_size(buffer_view_t *v);
+uint8_t buffer_view_get_u8(buffer_view_t *v, int32_t p);
+int8_t buffer_view_get_i8(buffer_view_t *v, int32_t p);
+uint16_t buffer_view_get_u16(buffer_view_t *v, int32_t p);
+int16_t buffer_view_get_i16(buffer_view_t *v, int32_t p);
+uint32_t buffer_view_get_u32(buffer_view_t *v, int32_t p);
+int32_t buffer_view_get_i32(buffer_view_t *v, int32_t p);
+float buffer_view_get_f32(buffer_view_t *v, int32_t p);
+void buffer_view_set_u8(buffer_view_t *v, int32_t p, uint8_t n);
+void buffer_view_set_i8(buffer_view_t *v, int32_t p, int8_t n);
+void buffer_view_set_u16(buffer_view_t *v, int32_t p, uint16_t n);
+void buffer_view_set_i16(buffer_view_t *v, int32_t p, uint16_t n);
+void buffer_view_set_u32(buffer_view_t *v, int32_t p, uint32_t n);
+void buffer_view_set_i32(buffer_view_t *v, int32_t p, int32_t n);
+void buffer_view_set_f32(buffer_view_t *v, int32_t p, float n);
