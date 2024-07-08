d46f4eb379154e2a8bb13f3389e6df61bc2b9f27
luboslenco <lubos.lenco@gmail.com>
2024-07-08 22:24:57 +0200

    Update kmake project

--- tools/kmake/project.js
+++ tools/kmake/project.js
@@ -5,17 +5,23 @@ project.addIncludeDir("../../sources/libs");
 project.addFile("../../sources/libs/quickjs/*.c");
 project.addFile("main.c");
 
-project.addLib("m");
-
 project.addDefine("environ=__environ");
 project.addDefine("sighandler_t=__sighandler_t");
 
 project.addDefine("WIN32_LEAN_AND_MEAN");
 project.addDefine("_WIN32_WINNT=0x0602");
-// /experimental:c11atomics
-// /std:c11
+
+if (platform === 'linux') {
+	project.addLib("m");
+}
+
+// Windows:
+// C/C++ - Command Line:
+// /experimental:c11atomics /std:c11
+// Linker - Command Line:
 // /subsystem:console
 
+// QuickJS changes:
 // quickjs-libc.c#85 (fixes "import * as os from 'os';" crash):
 // #define USE_WORKER -> //#define USE_WORKER
 // "quickjs.h#259" (fixes "Maximum call stack size exceeded" in minits):
