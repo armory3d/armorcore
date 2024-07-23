f1edeb3e7ecd81ba2baa28f2dc9969264ff4c617
luboslenco <lubos.lenco@gmail.com>
2024-07-24 00:11:37 +0200

    Fix alt key release on linux

--- sources/backends/linux/kinc/backend/x11/system.c.h
+++ sources/backends/linux/kinc/backend/x11/system.c.h
@@ -539,6 +539,7 @@ bool kinc_x11_handle_messages() {
 				KEY(XK_Control_L, KINC_KEY_CONTROL)
 				KEY(XK_Control_R, KINC_KEY_CONTROL)
 				KEY(XK_Alt_L, KINC_KEY_ALT)
+				KEY(XK_ISO_Prev_Group, KINC_KEY_ALT) //// XK_ISO_Prev_Group received instead of XK_Alt_L?
 				KEY(XK_Alt_R, KINC_KEY_ALT)
 				KEY(XK_Delete, KINC_KEY_DELETE)
 				KEY(XK_comma, KINC_KEY_COMMA)
