c0c5a5c118ef2a2f9ed78cdd79b745bd5ea53090
luboslenco <lubos.lenco@gmail.com>
2024-09-19 16:56:29 +0200

    Make temp string bigger

--- sources/iron.h
+++ sources/iron.h
@@ -185,12 +185,12 @@ int last_window_width = 0;
 int last_window_height = 0;
 #endif
 
-char temp_string[4096];
+char temp_string[1024 * 8];
 char temp_string_vs[1024 * 128];
 char temp_string_fs[1024 * 128];
 char temp_string_vstruct[4][32][32];
 #ifdef KINC_WINDOWS
-wchar_t temp_wstring[4096];
+wchar_t temp_wstring[1024 * 8];
 bool show_window = false;
 #endif
 
