7149650b8c4784035cd04b440f7a92987f4b28fb
luboslenco <lubos.lenco@gmail.com>
2024-09-24 20:35:57 +0200

    Cleanup

--- sources/ts/iron.ts
+++ sources/ts/iron.ts
@@ -399,3 +399,170 @@ declare type audio_channel_t = {
 	volume: f32;
 	pitch: f32;
 };
+
+declare type quat_t = {
+	x: f32;
+	y: f32;
+	z: f32;
+	w: f32;
+};
+
+declare function quat_create(x: f32 = 0.0, y: f32 = 0.0, z: f32 = 0.0, w: f32 = 1.0): quat_t;
+declare function quat_from_axis_angle(axis: vec4_t, angle: f32): quat_t;
+declare function quat_from_mat(m: mat4_t): quat_t;
+declare function quat_from_rot_mat(m: mat4_t): quat_t;
+declare function quat_mult(a: quat_t, b: quat_t): quat_t;
+declare function quat_norm(q: quat_t): quat_t;
+declare function quat_clone(q: quat_t): quat_t;
+declare function quat_get_euler(q: quat_t): vec4_t;
+declare function quat_from_euler(x: f32, y: f32, z: f32): quat_t;
+declare function quat_lerp(from: quat_t, to: quat_t, s: f32): quat_t;
+declare function quat_dot(a: quat_t, b: quat_t): f32;
+declare function quat_from_to(v0: vec4_t, v1: vec4_t): quat_t;
+
+declare type mat3_t = {
+	m?: f32_ptr;
+	m00: f32;
+	m01: f32;
+	m02: f32;
+	m10: f32;
+	m11: f32;
+	m12: f32;
+	m20: f32;
+	m21: f32;
+	m22: f32;
+};
+
+declare function mat3_create(_00: f32, _10: f32, _20: f32,
+							 _01: f32, _11: f32, _21: f32,
+							 _02: f32, _12: f32, _22: f32): mat3_t;
+declare function mat3_identity(): mat3_t;
+declare function mat3_translation(x: f32, y: f32): mat3_t;
+declare function mat3_rotation(alpha: f32): mat3_t;
+declare function mat3_set_from4(m4: mat4_t): mat3_t;
+declare function mat3_multmat(a: mat3_t, b: mat3_t): mat3_t;
+declare function mat3_nan(): mat3_t;
+declare function mat3_isnan(m: mat3_t): bool;
+
+declare type mat4_t = {
+	m?: f32_ptr;
+	m00: f32;
+	m01: f32;
+	m02: f32;
+	m03: f32;
+	m10: f32;
+	m11: f32;
+	m12: f32;
+	m13: f32;
+	m20: f32;
+	m21: f32;
+	m22: f32;
+	m23: f32;
+	m30: f32;
+	m31: f32;
+	m32: f32;
+	m33: f32;
+};
+
+declare type mat4_decomposed_t = {
+	loc: vec4_t;
+	rot: quat_t;
+	scl: vec4_t;
+}
+
+declare function mat4_create(_00: f32, _10: f32, _20: f32, _30: f32,
+							 _01: f32, _11: f32, _21: f32, _31: f32,
+							 _02: f32, _12: f32, _22: f32, _32: f32,
+							 _03: f32, _13: f32, _23: f32, _33: f32): mat4_t;
+declare function mat4_identity(): mat4_t;
+declare function mat4_from_f32_array(a: f32_array_t, offset: i32 = 0): mat4_t;
+declare function mat4_persp(fov_y: f32, aspect: f32, zn: f32, zf: f32): mat4_t;
+declare function mat4_ortho(left: f32, right: f32, bottom: f32, top: f32, znear: f32, zfar: f32): mat4_t;
+declare function mat4_rot_z(alpha: f32): mat4_t;
+declare function mat4_compose(loc: vec4_t, rot: quat_t, scl: vec4_t): mat4_t;
+declare function mat4_decompose(m: mat4_t): mat4_decomposed_t;
+declare function mat4_set_loc(m: mat4_t, v: vec4_t): mat4_t;
+declare function mat4_from_quat(q: quat_t): mat4_t;
+declare function mat4_init_translate(x: f32, y: f32, z: f32): mat4_t;
+declare function mat4_translate(m: mat4_t, x: f32, y: f32, z: f32): mat4_t;
+declare function mat4_scale(m: mat4_t, v: vec4_t): mat4_t;
+declare function mat4_mult_mat3x4(a: mat4_t, b: mat4_t): mat4_t;
+declare function mat4_mult_mat(a: mat4_t, b: mat4_t): mat4_t;
+declare function mat4_inv(a: mat4_t): mat4_t;
+declare function mat4_transpose(m: mat4_t): mat4_t;
+declare function mat4_transpose3x3(m: mat4_t): mat4_t;
+declare function mat4_clone(m: mat4_t): mat4_t;
+declare function mat4_get_loc(m: mat4_t): vec4_t;
+declare function mat4_get_scale(m: mat4_t): vec4_t;
+declare function mat4_mult(m: mat4_t, s: f32): mat4_t;
+declare function mat4_to_rot(m: mat4_t): mat4_t;
+declare function mat4_right(m: mat4_t): vec4_t;
+declare function mat4_look(m: mat4_t): vec4_t;
+declare function mat4_up(m: mat4_t): vec4_t;
+declare function mat4_to_f32_array(m: mat4_t): f32_array_t;
+declare function mat4_cofactor(m0: f32, m1: f32, m2: f32, m3: f32, m4: f32, m5: f32, m6: f32, m7: f32, m8: f32): f32;
+declare function mat4_determinant(m: mat4_t): f32;
+declare function mat4_nan(): mat4_t;
+declare function mat4_isnan(m: mat4_t): bool;
+declare let mat4nan: mat4_t;
+
+type mat4_box_t = {
+	v: mat4_t;
+};
+
+declare type vec2_t = {
+	x: f32;
+	y: f32;
+};
+
+declare function vec2_create(x: f32 = 0.0, y: f32 = 0.0): vec2_t;
+declare function vec2_len(v: vec2_t): f32;
+declare function vec2_set_len(v: vec2_t, length: f32): vec2_t;
+declare function vec2_mult(v: vec2_t, f: f32): vec2_t;
+declare function vec2_add(a: vec2_t, b: vec2_t): vec2_t;
+declare function vec2_sub(a: vec2_t, b: vec2_t): vec2_t;
+declare function vec2_cross(a: vec2_t, b: vec2_t): f32;
+declare function vec2_norm(v: vec2_t): vec2_t;
+declare function vec2_dot(a: vec2_t, b: vec2_t): f32;
+declare function vec2_nan(): vec2_t;
+declare function vec2_isnan(a: vec2_t): bool;
+
+declare type vec4_t = {
+	x: f32;
+	y: f32;
+	z: f32;
+	w: f32;
+};
+
+declare function vec4_create(x: f32 = 0.0, y: f32 = 0.0, z: f32 = 0.0, w: f32 = 1.0): vec4_t;
+declare function vec4_cross(a: vec4_t, b: vec4_t): vec4_t;
+declare function vec4_add(a: vec4_t, b: vec4_t): vec4_t;
+declare function vec4_fadd(a: vec4_t, x: f32, y: f32, z: f32, w: f32 = 0.0): vec4_t;
+declare function vec4_norm(a: vec4_t): vec4_t;
+declare function vec4_mult(v: vec4_t, f: f32): vec4_t;
+declare function vec4_dot(a: vec4_t, b: vec4_t): f32;
+declare function vec4_clone(v: vec4_t): vec4_t;
+declare function vec4_lerp(from: vec4_t, to: vec4_t, s: f32): vec4_t;
+declare function vec4_apply_proj(a: vec4_t, m: mat4_t): vec4_t;
+declare function vec4_apply_mat(a: vec4_t, m: mat4_t): vec4_t;
+declare function vec4_apply_mat4(a: vec4_t, m: mat4_t): vec4_t;
+declare function vec4_apply_axis_angle(a: vec4_t, axis: vec4_t, angle: f32): vec4_t;
+declare function vec4_apply_quat(a: vec4_t, q: quat_t): vec4_t;
+declare function vec4_equals(a: vec4_t, b: vec4_t): bool;
+declare function vec4_almost_equals(a: vec4_t, b: vec4_t, prec: f32): bool;
+declare function vec4_len(a: vec4_t): f32;
+declare function vec4_sub(a: vec4_t, b: vec4_t): vec4_t;
+declare function vec4_exp(a: vec4_t): vec4_t;
+declare function vec4_dist(v1: vec4_t, v2: vec4_t): f32;
+declare function vec4_fdist(v1x: f32, v1y: f32, v1z: f32, v2x: f32, v2y: f32, v2z: f32): f32;
+declare function vec4_reflect(a: vec4_t, n: vec4_t): vec4_t;
+declare function vec4_clamp(a: vec4_t, min: f32, max: f32): vec4_t;
+declare function vec4_x_axis(): vec4_t;
+declare function vec4_y_axis(): vec4_t;
+declare function vec4_z_axis(): vec4_t;
+declare function vec4_nan(): vec4_t;
+declare function vec4_isnan(a: vec4_t): bool;
+
+type vec4_box_t = {
+	v: vec4_t;
+};
