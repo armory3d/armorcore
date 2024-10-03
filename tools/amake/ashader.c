8a4d307e0f67a6f6aabf76e2159124827ce02d76
luboslenco <lubos.lenco@gmail.com>
2024-10-03 21:43:55 +0200

    Cleanup

--- tools/amake/ashader.c
+++ tools/amake/ashader.c
@@ -26,12 +26,11 @@ static void write_attrib(char *file, int *output_len, const char *attrib, int in
 }
 
 char *hlsl_to_bin(char *source, char *shader_type, char *to) {
-
 	char *type;
-	if (strcmp(shader_type, "vert") == 0) {
+	if (string_equals(shader_type, "vert")) {
 		type = "vs_5_0";
 	}
-	else if (strcmp(shader_type, "frag") == 0) {
+	else if (string_equals(shader_type, "frag")) {
 		type = "ps_5_0";
 	}
 	else {
@@ -185,12 +184,13 @@ void hlslbin(const char *from, const char *to) {
 #endif
 
 static char out[128 * 1024];
+static int out_pos = 0;
 static char line[1024];
 static char tmp[1024];
 
 static char *buffer;
 static int buffer_size;
-static int pos = 0;
+static int buffer_pos = 0;
 
 typedef struct var {
 	char name[64];
@@ -270,39 +270,43 @@ float2 _getDimensions(texture2d<float> tex, uint lod) { return float2(tex.get_wi
 static char *read_line() {
 	int i = 0;
 	while (true) {
-		if (buffer[pos] == '\0') {
+		if (buffer[buffer_pos] == '\0') {
 			return NULL;
 		}
 
-		line[i] = buffer[pos];
+		line[i] = buffer[buffer_pos];
 		i++;
-		pos++;
+		buffer_pos++;
 
 		#ifdef _WIN32
-		if (buffer[pos - 1] == '\r') {
+		if (line[i - 1] == '\r') {
 			line[i - 1] = '\n';
-			pos++; // Skip \n
+			buffer_pos++; // Skip \n
 			break;
 		}
 		#endif
 
-		if (buffer[pos - 1] == '\n') {
+		if (line[i - 1] == '\n') {
 			break;
 		}
 	}
 
 	line[i] = '\0';
-
 	return &line[0];
 }
 
-static void add_includes(char *file, int off) {
+static void w(char *str) {
+	strcpy(out + out_pos, str);
+	out_pos += strlen(str);
+}
+
+static void write_includes(char *file, int off) {
 	char *_buffer = buffer;
-	int _pos = pos;
-	pos = off;
+	int _pos = buffer_pos;
+	buffer_pos = off;
 
 	FILE *fp = fopen(file, "rb");
-	fseek(fp , 0, SEEK_END);
+	fseek(fp, 0, SEEK_END);
 	int size = ftell(fp);
 	rewind(fp);
 	buffer = malloc(size + 1);
@@ -325,34 +329,37 @@ static void add_includes(char *file, int off) {
 			tmp[last] = '\0';
 			strcat(tmp, rel);
 
-			add_includes(tmp, 0);
+			write_includes(tmp, 0);
 			line[0] = '\0';
 		}
 
-		strcat(out, line);
+		w(line);
 	}
 
 	free(buffer);
 	buffer = _buffer;
-	pos = _pos;
+	buffer_pos = _pos;
 }
 
-static void add_header(char *shader_lang) {
-	if (strcmp(shader_lang, "glsl") == 0 || strcmp(shader_lang, "essl") == 0 || strcmp(shader_lang, "spirv") == 0) {
-		if (strcmp(shader_lang, "essl") == 0) {
-			strcpy(out, version_essl);
-			strcat(out, precision_essl);
+static void write_header(char *shader_lang) {
+	if (string_equals(shader_lang, "glsl") ||
+		string_equals(shader_lang, "essl") ||
+		string_equals(shader_lang, "spirv")
+	) {
+		if (string_equals(shader_lang, "essl")) {
+			w(version_essl);
+			w(precision_essl);
 		}
 		else {
-			strcpy(out, version_glsl);
+			w(version_glsl);
 		}
-		strcat(out, header_glsl);
+		w(header_glsl);
 	}
-	else if (strcmp(shader_lang, "hlsl") == 0) {
-		strcpy(out, header_hlsl);
+	else if (string_equals(shader_lang, "hlsl")) {
+		w(header_hlsl);
 	}
-	else if (strcmp(shader_lang, "msl") == 0) {
-		strcpy(out, header_msl);
+	else if (string_equals(shader_lang, "msl")) {
+		w(header_msl);
 	}
 }
 
@@ -362,127 +369,112 @@ static int sort_vars(const void *_a, const void *_b) {
 	return strcmp(a->name, b->name);
 }
 
-static char *add_inputs(char *line) {
-	memset(&inputs, 0, sizeof(inputs));
-	inputs_count = 0;
+static void read_vars(char *line, var_t *vars, int *vars_count, char *start) {
+	*vars_count = 0;
 
-	while (starts_with(line, "in ")) {
-		line = line + 3; // "in "
+	while (starts_with(line, start)) {
+		memset(&vars[*vars_count], 0, sizeof(var_t));
+		line = line + strlen(start);
 
 		int pos = string_index_of(line, " ");
-		strncat(inputs[inputs_count].type, line, pos);
+		strncat(vars[*vars_count].type, line, pos);
 		line += pos + 1;
 
 		pos = string_index_of(line, ";");
-		strncat(inputs[inputs_count].name, line, pos);
+		strncat(vars[*vars_count].name, line, pos);
 
-		inputs_count++;
-
+		(*vars_count)++;
 		line = read_line();
 	}
 
-	qsort(inputs, inputs_count, sizeof(inputs[0]), sort_vars);
+	qsort(vars, *vars_count, sizeof(var_t), sort_vars);
+}
 
-	strcat(out, "struct shader_input {\n");
+static char *write_inputs(char *line) {
+	read_vars(line, inputs, &inputs_count, "in ");
+
+	w("struct shader_input {\n");
 	for (int i = 0; i < inputs_count; ++i) {
-		strcat(out, "\t");
-		strcat(out, inputs[i].type);
-		strcat(out, " ");
-		strcat(out, inputs[i].name);
-		strcat(out, ": TEXCOORD");
-		strcat(out, numbers[i]);
-		strcat(out, ";\n");
+		w("\t");
+		w(inputs[i].type);
+		w(" ");
+		w(inputs[i].name);
+		w(": TEXCOORD");
+		w(numbers[i]);
+		w(";\n");
 	}
 
 	if (string_index_of(buffer, "gl_VertexID") > -1) {
-		strcat(out, "\tuint gl_VertexID: SV_VertexID;\n");
+		w("\tuint gl_VertexID: SV_VertexID;\n");
 	}
-
 	if (string_index_of(buffer, "gl_InstanceID") > -1) {
-		strcat(out, "\tuint gl_InstanceID: SV_InstanceID;\n");
+		w("\tuint gl_InstanceID: SV_InstanceID;\n");
 	}
 
-	strcat(out, "};\n");
+	w("};\n");
 
 	return line;
 }
 
-static char *add_outputs(char *line, char *shader_type) {
-	memset(&outputs, 0, sizeof(outputs));
-	outputs_count = 0;
+static char *write_outputs(char *line, char *shader_type) {
+	read_vars(line, outputs, &outputs_count, "out ");
 
-	while (starts_with(line, "out ")) {
-		line = line + 4; // "out "
-
-		int pos = string_index_of(line, " ");
-		strncat(outputs[outputs_count].type, line, pos);
-		line += pos + 1;
-
-		pos = string_index_of(line, ";");
-		strncat(outputs[outputs_count].name, line, pos);
-
-		outputs_count++;
-
-		line = read_line();
-	}
-
-	qsort(outputs, outputs_count, sizeof(outputs[0]), sort_vars);
-
-	strcat(out, "struct shader_output {\n");
+	w("struct shader_output {\n");
 	for (int i = 0; i < outputs_count; ++i) {
-		strcat(out, "\t");
-		strcat(out, outputs[i].type);
-		strcat(out, " ");
-		strcat(out, outputs[i].name);
+		w("\t");
+		w(outputs[i].type);
+		w(" ");
+		w(outputs[i].name);
 
-		if (strcmp(shader_type, "vert") == 0) {
-			strcat(out, ": TEXCOORD");
+		if (string_equals(shader_type, "vert")) {
+			w(": TEXCOORD");
 		}
 		else {
-			strcat(out, ": SV_TARGET");
+			w(": SV_TARGET");
 		}
 
-		strcat(out, numbers[i]);
-		strcat(out, ";\n");
+		w(numbers[i]);
+		w(";\n");
 	}
 
-	if (strcmp(shader_type, "vert") == 0) {
-		strcat(out, "\tvec4 gl_Position: SV_POSITION;\n");
+	if (string_equals(shader_type, "vert")) {
+		w("\tvec4 gl_Position: SV_POSITION;\n");
 	}
 
-	strcat(out, "};\n");
+	w("};\n");
 
 	return line;
 }
 
 static void write_return(const char *shader_type) {
-	strcat(out, "\tshader_output stage_output;\n");
-	if (strcmp(shader_type, "vert") == 0) {
-		strcat(out, "\tgl_Position.z = (gl_Position.z + gl_Position.w) * 0.5;\n");
-		strcat(out, "\tstage_output.gl_Position = gl_Position;\n");
+	w("\tshader_output stage_output;\n");
+	if (string_equals(shader_type, "vert")) {
+		w("\tgl_Position.z = (gl_Position.z + gl_Position.w) * 0.5;\n");
+		w("\tstage_output.gl_Position = gl_Position;\n");
 	}
 	for (int i = 0; i < outputs_count; ++i) {
-		strcat(out, "\tstage_output.");
-		strcat(out, outputs[i].name);
-		if (out[strlen(out) - 1] == ']') {
-			out[strlen(out) - 3] = '\0';
+		w("\tstage_output.");
+		w(outputs[i].name);
+		if (out[out_pos - 1] == ']') {
+			out_pos -= 3; // Erase [i]
 		}
-		strcat(out, " = ");
-		strcat(out, outputs[i].name);
-		if (out[strlen(out) - 1] == ']') {
-			out[strlen(out) - 3] = '\0';
+		w(" = ");
+		w(outputs[i].name);
+		if (out[out_pos - 1] == ']') {
+			out_pos -= 3;
 		}
-		strcat(out, ";\n");
+		w(";\n");
 	}
-	strcat(out, "\treturn stage_output;\n");
+	w("\treturn stage_output;\n");
 }
 
 static void to_hlsl(const char *shader_type) {
 	buffer = malloc(128 * 1024);
 	strcpy(buffer, out);
 	buffer_size = strlen(buffer);
+	buffer_pos = 0;
 	out[0] = '\0';
-	pos = 0;
+	out_pos = 0;
 
 	while (true) {
 		char *line = read_line();
@@ -491,38 +483,37 @@ static void to_hlsl(const char *shader_type) {
 		}
 
 		if (starts_with(line, "in ")) {
-			line = add_inputs(line);
+			line = write_inputs(line);
 
 			for (int i = 0; i < inputs_count; ++i) {
-				strcat(out, "static ");
-				strcat(out, inputs[i].type);
-				strcat(out, " ");
-				strcat(out, inputs[i].name);
-				strcat(out, ";\n");
+				w("static ");
+				w(inputs[i].type);
+				w(" ");
+				w(inputs[i].name);
+				w(";\n");
 			}
 
 			if (string_index_of(buffer, "gl_VertexID") > -1) {
-				strcat(out, "static uint gl_VertexID;\n");
+				w("static uint gl_VertexID;\n");
 			}
-
 			if (string_index_of(buffer, "gl_InstanceID") > -1) {
-				strcat(out, "static uint gl_InstanceID;\n");
+				w("static uint gl_InstanceID;\n");
 			}
 		}
 
 		if (starts_with(line, "out ")) {
-			line = add_outputs(line, shader_type);
+			line = write_outputs(line, shader_type);
 
-			if (strcmp(shader_type, "vert") == 0) {
-				strcat(out, "static vec4 gl_Position;\n");
+			if (string_equals(shader_type, "vert")) {
+				w("static vec4 gl_Position;\n");
 			}
 
 			for (int i = 0; i < outputs_count; ++i) {
-				strcat(out, "static ");
-				strcat(out, outputs[i].type);
-				strcat(out, " ");
-				strcat(out, outputs[i].name);
-				strcat(out, ";\n");
+				w("static ");
+				w(outputs[i].type);
+				w(" ");
+				w(outputs[i].name);
+				w(";\n");
 			}
 		}
 
@@ -532,16 +523,19 @@ static void to_hlsl(const char *shader_type) {
 			strcpy(tmp, "SamplerState ");
 			strncat(tmp, line + last + 1, len - last - 3); // - ";\n"
 			strcat(tmp, "_sampler;\n");
-			strcat(out, tmp);
+			w(tmp);
 		}
 
 		if (starts_with(line, "const ")) {
-			strcat(out, "static ");
+			w("static ");
 		}
 
-		if (starts_with(line, "vec") || starts_with(line, "float ") || starts_with(line, "int ")) {
+		if (starts_with(line, "vec") ||
+			starts_with(line, "float ") ||
+			starts_with(line, "int ")
+		) {
 			if (ends_with(line, ";\n")) {
-				strcat(out, "static ");
+				w("static ");
 			}
 		}
 
@@ -551,35 +545,40 @@ static void to_hlsl(const char *shader_type) {
 		}
 
 		if (starts_with(line, "void main")) {
-			strcat(out, "shader_output main(shader_input stage_input) {\n");
+			w("shader_output main(shader_input stage_input) {\n");
 			line = "";
 
 			for (int i = 0; i < inputs_count; ++i) {
-				strcat(out, "\t");
-				strcat(out, inputs[i].name);
-				strcat(out, " = stage_input.");
-				strcat(out, inputs[i].name);
-				strcat(out, ";\n");
+				w("\t");
+				w(inputs[i].name);
+				w(" = stage_input.");
+				w(inputs[i].name);
+				w(";\n");
 			}
 
 			if (string_index_of(buffer, "gl_VertexID") > -1) {
-				strcat(out, "\tgl_VertexID = stage_input.gl_VertexID;\n");
+				w("\tgl_VertexID = stage_input.gl_VertexID;\n");
 			}
 
 			if (string_index_of(buffer, "gl_InstanceID") > -1) {
-				strcat(out, "\tgl_InstanceID = stage_input.gl_InstanceID;\n");
+				w("\tgl_InstanceID = stage_input.gl_InstanceID;\n");
 			}
 		}
 
-		if (starts_with(line, "}") && pos == buffer_size) {
+		if (starts_with(line, "}") && buffer_pos == buffer_size) {
 			write_return(shader_type);
 		}
 
-		strcat(out, line);
+		w(line);
 	}
 
 	free(buffer);
 }
+
+#ifdef __APPLE__
+static void to_msl(const char *shader_type) {
+}
+#endif
 
 int ashader(char *shader_lang, char *from, char *to) {
 	// shader_lang == glsl || essl || hlsl || msl || spirv
@@ -597,17 +596,21 @@ int ashader(char *shader_lang, char *from, char *to) {
 	from = from_;
 	#endif
 
-	add_header(shader_lang);
-	add_includes(from, 13); // Skip #version 450\n
+	out[0] = '\0';
+	out_pos = 0;
+	write_header(shader_lang);
+	write_includes(from, 13); // Skip #version 450\n
 
-	if (strcmp(shader_lang, "glsl") == 0 || strcmp(shader_lang, "essl") == 0) {
+	if (string_equals(shader_lang, "glsl") ||
+		string_equals(shader_lang, "essl")
+	) {
 		FILE *fp = fopen(to, "wb");
 		fwrite(out, 1, strlen(out), fp);
 		fclose(fp);
 	}
 
 	#ifdef __linux__
-	else if (strcmp(shader_lang, "spirv") == 0) {
+	else if (string_equals(shader_lang, "spirv")) {
 		char *buf = malloc(1024 * 1024);
 		int buf_len;
 		krafix_compile(out, buf, &buf_len, "spirv", "linux", shader_type, -1);
@@ -619,12 +622,12 @@ int ashader(char *shader_lang, char *from, char *to) {
 	#endif
 
 	#ifdef _WIN32
-	else if (strcmp(shader_lang, "hlsl") == 0) {
+	else if (string_equals(shader_lang, "hlsl")) {
 		to_hlsl(shader_type);
 
-		// FILE *fp = fopen(to, "wb");
-		// fwrite(out, 1, strlen(out), fp); // Write .hlsl
-		// fclose(fp);
+		FILE *fp = fopen(to, "wb");
+		fwrite(out, 1, strlen(out), fp); // Write .hlsl
+		fclose(fp);
 
 		char to_[512];
 		strcpy(to_, to);
@@ -635,8 +638,8 @@ int ashader(char *shader_lang, char *from, char *to) {
 	#endif
 
 	#ifdef __APPLE__
-	else if (strcmp(shader_lang, "msl") == 0) {
-		// to_msl();
+	else if (string_equals(shader_lang, "msl")) {
+		to_msl(shader_type);
 	}
 	#endif
 
